0e2fa97ceb72af00232cd910aa27bac76cabbb21
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-09-19 10:11:09 -0300

    Creating cryptography class

---
+++ apiweb.eventPlus.codeFirst/Utils/Cryptography.cs
@@ -0,0 +1,15 @@
+﻿namespace apiweb.eventPlus.codeFirst.Utils
+{
+    public static class Cryptography
+    {
+        public static string GetHashByPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool IsEqualHashes(string password, string databasePasswordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, databasePasswordHash);
+        }
+    }
+}
